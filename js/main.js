7e5a6f32008ed2ff5322e39d1e58668fe5a94877
Rishab Goyal <rishabgoyal432@gmail.com>
2020-07-03 23:50:18 +0530

    Navbar
    Completed work of responsive navbar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let x = document.getElementById("myTopNav");
+function myFuction() {
+    if(x.style.height !== "auto"){
+        x.style.height = "auto";
+    }
+        else{
+            x.style.height = "51px";
+        }
+}
