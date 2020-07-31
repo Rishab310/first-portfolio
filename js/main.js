b948ea28d965561977e3fc0b4f4a3667abae271d
Rishab Goyal <rishabgoyal432@gmail.com>
2020-07-31 21:02:50 +0530

    Major

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 let x = document.getElementById("myTopNav");
 let welcome = document.getElementById("welcome");
 let menu = document.getElementById("navbut");
-menu.onclick =  function() {
-    if(x.style.height !== "auto") {
-        x.style.height = "auto";
-        welcome.style.display = "none";
-    }
-        else {
-            x.style.height = "51px";
-        }
-    }
-window.onscroll = function() {
-    if (window.pageYOffset > 60) {
-        welcome.style.display = "none"
-    }
-    else {
-        welcome.style.display = "inline";
-    }
-}
+menu.onclick = function () {
+  if (x.style.height !== "auto") {
+    x.style.height = "auto";
+    welcome.style.display = "none";
+  } else {
+    x.style.height = "51px";
+  }
+};
+window.onscroll = function () {
+  if (window.pageYOffset > 60) {
+    welcome.style.display = "none";
+  } else {
+    welcome.style.display = "inline";
+  }
+};
